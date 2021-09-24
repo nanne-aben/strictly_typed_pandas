c2a60d7ad936f4655a23b8c4bad0e63a1d9a7173
Nanne Aben <nanneaben@nannes-mbp-2.home>
2021-09-24 06:28:08 +0200

    update

--- strictly_typed_pandas/core/dataset/dataset.py
+++ strictly_typed_pandas/core/dataset/dataset.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 from typing import Generic, List, TypeVar, Union, get_type_hints, overload
-from pandas._typing import FrameOrSeries
 
 from strictly_typed_pandas.core.dataset.base import DataSetBase
 from strictly_typed_pandas.core.join import Join
 from strictly_typed_pandas.core.exact_match_for_protocols import ExactMatchForProtocols
 from strictly_typed_pandas.core.validate_schema import validate_schema
 from strictly_typed_pandas.core.create_empty_dataframe import create_empty_dataframe
+from strictly_typed_pandas.utils.pandas_types import FrameOrSeries
 
 
 _Schema = TypeVar("_Schema")
