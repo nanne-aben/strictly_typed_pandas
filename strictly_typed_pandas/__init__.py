a4c804b56534873efdc701815ff5b4a06cc36b86
nanne-aben <47976799+nanne-aben@users.noreply.github.com>
2023-08-25 09:04:57 +0200

    update (#69)

--- strictly_typed_pandas/__init__.py
+++ strictly_typed_pandas/__init__.py
@@ -1,2 +1,4 @@
-from strictly_typed_pandas.dataset import DataSet, IndexedDataSet  # noqa: F401
+from strictly_typed_pandas.dataset import DataSet, IndexedDataSet
 import strictly_typed_pandas.typeguard  # noqa: F401
+
+__all__ = ["DataSet", "IndexedDataSet"]
