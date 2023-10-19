fe97d1590176955d8eb2cb534d1cb178472fbf24
caneff <caneff@gmail.com>
2023-10-19 04:40:38 -0400

    Clean up files so that pre-commit hooks are clean. (#95)
    No more errors or warnings
    
    Co-authored-by: nanne-aben <47976799+nanne-aben@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# This is a configuration file we shouldn't be checking it.
+# mypy: ignore-errors
 
 # -- Project information -----------------------------------------------------
 
