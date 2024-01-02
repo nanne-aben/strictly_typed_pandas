5e30acd530ded8cc8c9a7aa7bba8392ee084a9b6
Will Hardy <willhardy@users.noreply.github.com>
2024-01-02 15:44:27 +0100

    Allow typeguard to be installed at same time.
    If typeguard is not installed separately, process the packages listed under --typeguard-packages using the vendored typeguard. Otherwise, process only what is listed in --stp-typeguard-packages and leave the --typeguard-packages option to be processed by the installed version of typeguard. To avoid the ambiguity of double processing, an exception is raised if a package is listed in both options.

--- strictly_typed_pandas/pytest_plugin.py
+++ strictly_typed_pandas/pytest_plugin.py
@@ -12,27 +12,56 @@ except ImportError:
 
 
 def pytest_addoption(parser):
-    if TYPEGUARD_INSTALLED:
-        return
-    group = parser.getgroup("typeguard")
+    group = parser.getgroup("stp_typeguard")
     group.addoption(
-        "--typeguard-packages",
+        "--stp-typeguard-packages",
         action="store",
-        help="comma separated name list of packages and modules to " "instrument for type checking",
+        help=(
+            "comma separated name list of packages and modules to "
+            "instrument for type checking by strictly typed pandas"
+        ),
     )
+    if not TYPEGUARD_INSTALLED:
+        group = parser.getgroup("typeguard")
+        group.addoption(
+            "--typeguard-packages",
+            action="store",
+            help=(
+                "comma separated name list of packages and modules to "
+                "instrument for type checking"
+            ),
+        )
+
+
+def _parse_packages(val: str) -> list[str]:
+    if val is None or not val.strip():
+        return []
+    return [pkg.strip() for pkg in val.split(",")]
 
 
 def pytest_configure(config):
-    value = config.getoption("typeguard_packages")
-    if not value:
+    packages = _parse_packages(config.getoption("stp_typeguard_packages"))
+    typeguard_packages = _parse_packages(config.getoption("typeguard_packages"))
+
+    packages_in_both = set(packages) & set(typeguard_packages)
+    if packages_in_both:
+        raise RuntimeError(
+            "If you are going to use both --stp-typeguard-packages "
+            "and --typeguard-packages at the same time, "
+            "please don't list the same package in both options: "
+            f"{', '.join(packages_in_both)}"
+        )
+
+    if not TYPEGUARD_INSTALLED:
+        packages.extend(typeguard_packages)
+
+    if not packages:
         return
 
-    packages = [pkg.strip() for pkg in value.split(",")]
-
-    already_imported_packages = sorted(package for package in packages if package in sys.modules)
+    already_imported_packages = sorted(p for p in packages if p in sys.modules)
     if already_imported_packages:
         message = (
-            "strictly_typed_pandas cannot check these packages with typeguard because "
+            "strictly_typed_pandas cannot check these packages because "
             "they are already imported: {}"
         )
         raise RuntimeError(message.format(", ".join(already_imported_packages)))
